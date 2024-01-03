85b92e3e4ef3bc41218d39fa3ed3f53a5c63c6e5
Tim Harding <tim@timharding.co>
2024-01-03 15:55:06 -0800

    Implemented DoubleEndedIterator

--- src/soa.rs
+++ src/soa.rs
@@ -396,6 +396,20 @@ where
     }
 }
 
+impl<T> DoubleEndedIterator for IntoIter<T>
+where
+    T: Soapy,
+{
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.start >= self.end {
+            None
+        } else {
+            self.end -= 1;
+            Some(unsafe { self.raw.get(self.end) })
+        }
+    }
+}
+
 impl<T> Drop for IntoIter<T>
 where
     T: Soapy,
