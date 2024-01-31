5ea777fb404ac65c1cd45de0e5c0e0f45d98a9fa
Tim Harding <tim@timharding.co>
2024-01-30 18:42:19 -0800

    Added a GitHub issue for the soapy_shared reasoning

--- soapy-shared/src/lib.rs
+++ soapy-shared/src/lib.rs
@@ -6,7 +6,8 @@
 //! Unfortunately, if a trait is part of the current crate, the path has to be
 //! prefixed with `crate::` rather than `::my_crate_name::`, so it would not be
 //! possible to implement these traits for any types in `soapy` for testing.
-//! That is why these are split out into a subcrate.
+//! That is why these are split out into a subcrate. See the [tracking
+//! issue](https://github.com/rust-lang/rust/issues/54363) for more details.
 
 mod raw_soa;
 pub use raw_soa::RawSoa;
