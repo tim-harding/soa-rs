5564e658e2c58c78069463ca5a8b5d3c405c90ce
Tim Harding <tim@timharding.co>
2024-02-06 13:36:35 -0800

    Added configuration for benchmarking

--- soapy-testing/benches/benchmark.rs
+++ soapy-testing/benches/benchmark.rs
@@ -1,3 +1,6 @@
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand::{RngCore, SeedableRng};
 use soapy::{Soa, Soapy};
