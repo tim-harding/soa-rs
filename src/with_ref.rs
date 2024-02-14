f422de06573670f0e5acd500bfd760e5649ecbe4
Tim Harding <tim@timharding.co>
2024-02-13 18:52:51 -0800

    WithRef doc improvements

--- src/with_ref.rs
+++ src/with_ref.rs
@@ -1,23 +1,25 @@
-/// Similar to [`AsRef`], except that instead of returning `&T`, it accepts
+/// Similar to [`AsRef`], except that instead of returning `&T`, it accepts a
 /// closure that takes `&T`.
 ///
-/// This is required for getting a `&T` from a [`Soapy::Ref`] or
-/// [`Soapy::RefMut`] because these types only contain references to each of
+/// This is required for getting a `&T` from a [`Ref`] or
+/// [`RefMut`] because these types only contain references to each of
 /// `T`'s fields. In order to get a reference to `T`, `T` must first be
 /// constructed on the stack, but returning that reference is a lifetime
-/// violation. Therefore, we must invert the control flow. Note that the same
-/// idea for getting `&mut T` is less effective, as we would have to write all
-/// the fields to `T` back to their actual storage locations in the `Soa` after
-/// the closure, even if only some of the fields were modified.
+/// violation. Therefore, we must invert the control flow.
 ///
-/// This trait also provides various convenience methods that are equivalent
-/// to common [`std`] trait methods. Ideally, we would like to write blanket
-/// implementations for all types that implement [`WithRef`]. Unfortunately,
-/// doing so is a violation of the orphan rule, so this is the next best thing.
-/// All of these convenience methods forward to the implementations on `T`.
 ///
-/// [`Soapy::Ref`]: crate::Soapy::Ref
-/// [`Soapy::RefMut`]: crate::Soapy::RefMut
+/// This trait also provides the [`cloned`] and [`copied`] convenience methods
+/// for getting a clone of `T` from `Ref<T>` or `RefMut<T>`.
+///
+/// Note that the same idea applied to `&mut T` is less effective, as we would
+/// have to write all the fields to `T` back to their actual storage locations
+/// in the `Soa` after the closure, even if only some of the fields were
+/// modified.
+///
+/// [`Ref`]: crate::Ref
+/// [`RefMut`]: crate::RefMut
+/// [`copied`]: WithRef::copied
+/// [`cloned`]: WithRef::cloned
 pub trait WithRef {
     /// The type to be borrowed.
     type Item;
