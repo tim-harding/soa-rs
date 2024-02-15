40d9ddc16c0282ed205273672da2885f0ea37933
Tim Harding <tim@timharding.co>
2024-02-15 14:49:39 -0800

    More explicit equality impls

--- src/eq_impl.rs
+++ src/eq_impl.rs
@@ -1,49 +1,50 @@
-macro_rules! array {
+macro_rules! uni {
     ($u:ty, $t:ty $(,$N:tt)?) => {
         impl<'a, T $(,const $N: usize)?> PartialEq<$t> for $u
         where
-            T: 'a + Soapy + PartialEq,
+            T: Soapy + PartialEq,
         {
             fn eq(&self, other: &$t) -> bool {
-                self.as_ref().eq(other)
+                let me: &Slice<T> = self.as_ref();
+                me.eq(other)
             }
         }
 
-        impl<'a, T $(,const $N: usize)?> PartialEq<$u> for $t
+    };
+}
+
+macro_rules! bi {
+    ($t:ty, $u:ty $(,$N:tt)?) => {
+        $crate::eq_impl::uni!($t, $u $(,$N)?);
+
+        impl<'a, T $(,const $N: usize)?> PartialEq<$t> for $u
         where
-            T: 'a + Soapy + PartialEq,
+            T: Soapy + PartialEq,
         {
-            fn eq(&self, other: &$u) -> bool {
+            fn eq(&self, other: &$t) -> bool {
                 other.eq(self)
             }
         }
     };
 }
 
-pub(crate) use array;
-
 macro_rules! impl_for {
     ($t:ty) => {
-        $crate::eq_impl::array!($t, Vec<T>);
-        $crate::eq_impl::array!($t, [T]);
-        $crate::eq_impl::array!($t, &[T]);
-        $crate::eq_impl::array!($t, &mut [T]);
-        $crate::eq_impl::array!($t, [T; N], N);
-        $crate::eq_impl::array!($t, &[T; N], N);
-        $crate::eq_impl::array!($t, &mut [T; N], N);
-
-        impl<'a, T, R> PartialEq<R> for $t
-        where
-            T: 'a + Soapy + PartialEq,
-            R: AsRef<Slice<T>>,
-        {
-            fn eq(&self, other: &R) -> bool {
-                self.as_ref().eq(other.as_ref())
-            }
-        }
-
-        impl<'a, T> Eq for $t where T: 'a + Soapy + Eq {}
+        $crate::eq_impl::bi!($t, Vec<T>);
+        $crate::eq_impl::bi!($t, [T]);
+        $crate::eq_impl::bi!($t, &[T]);
+        $crate::eq_impl::bi!($t, &mut [T]);
+        $crate::eq_impl::bi!($t, [T; N], N);
+        $crate::eq_impl::bi!($t, &[T; N], N);
+        $crate::eq_impl::bi!($t, &mut [T; N], N);
+        $crate::eq_impl::bi!($t, Slice<T>);
+        $crate::eq_impl::uni!($t, SliceRef<'_, T>);
+        $crate::eq_impl::uni!($t, SliceMut<'_, T>);
+        $crate::eq_impl::uni!($t, Soa<T>);
+        impl<'a, T> Eq for $t where T: Soapy + Eq {}
     };
 }
 
+pub(crate) use bi;
 pub(crate) use impl_for;
+pub(crate) use uni;
