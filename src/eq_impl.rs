b505df0a9b27750daa3157b15cde797cb921d59d
Tim Harding <tim@timharding.co>
2024-02-15 17:58:19 -0800

    Moved eq_impl generic bounds to invocation

--- src/eq_impl.rs
+++ src/eq_impl.rs
@@ -1,26 +1,19 @@
 macro_rules! uni {
-    ($t:ty, $u:ty $(,$N:tt)?) => {
-        impl<T $(,const $N: usize)?> PartialEq<$u> for $t
-        where
-            T: Soapy + PartialEq,
-        {
+    ($t:ty, $u:ty, $($b:tt)*) => {
+        impl<$($b)*> PartialEq<$u> for $t {
             fn eq(&self, other: &$u) -> bool {
                 let me: &Slice<T> = self.as_ref();
                 me.eq(other)
             }
         }
-
     };
 }
 
 macro_rules! bi {
-    ($t:ty, $u:ty $(,$N:tt)?) => {
-        $crate::eq_impl::uni!($t, $u $(,$N)?);
+    ($t:ty, $u:ty, $($b:tt)*) => {
+        $crate::eq_impl::uni!($t, $u, $($b)*);
 
-        impl<T $(,const $N: usize)?> PartialEq<$t> for $u
-        where
-            T: Soapy + PartialEq,
-        {
+        impl<$($b)*> PartialEq<$t> for $u {
             fn eq(&self, other: &$t) -> bool {
                 other.eq(self)
             }
@@ -30,17 +23,17 @@ macro_rules! bi {
 
 macro_rules! impl_for {
     ($t:ty) => {
-        $crate::eq_impl::bi!($t, Vec<T>);
-        $crate::eq_impl::bi!($t, [T]);
-        $crate::eq_impl::bi!($t, &[T]);
-        $crate::eq_impl::bi!($t, &mut [T]);
-        $crate::eq_impl::bi!($t, [T; N], N);
-        $crate::eq_impl::bi!($t, &[T; N], N);
-        $crate::eq_impl::bi!($t, &mut [T; N], N);
-        $crate::eq_impl::bi!($t, Slice<T>);
-        $crate::eq_impl::uni!($t, SliceRef<'_, T>);
-        $crate::eq_impl::uni!($t, SliceMut<'_, T>);
-        $crate::eq_impl::uni!($t, Soa<T>);
+        $crate::eq_impl::bi!($t, Vec<T>, T: Soapy + PartialEq);
+        $crate::eq_impl::bi!($t, [T], T: Soapy + PartialEq);
+        $crate::eq_impl::bi!($t, &[T], T: Soapy + PartialEq);
+        $crate::eq_impl::bi!($t, &mut [T], T: Soapy + PartialEq);
+        $crate::eq_impl::bi!($t, [T; N], T: Soapy + PartialEq, const N: usize);
+        $crate::eq_impl::bi!($t, &[T; N], T: Soapy + PartialEq, const N: usize);
+        $crate::eq_impl::bi!($t, &mut [T; N], T: Soapy + PartialEq, const N: usize);
+        $crate::eq_impl::bi!($t, Slice<T>, T: Soapy + PartialEq);
+        $crate::eq_impl::uni!($t, SliceRef<'_, T>, T: Soapy + PartialEq);
+        $crate::eq_impl::uni!($t, SliceMut<'_, T>, T: Soapy + PartialEq);
+        $crate::eq_impl::uni!($t, Soa<T>, T: Soapy + PartialEq);
         impl<'a, T> Eq for $t where T: Soapy + Eq {}
     };
 }
